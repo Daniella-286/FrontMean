148f827eca28958c91f1e530a3891550c8d34d67
Daniella-286 <119035286+Daniella-286@users.noreply.github.com>
2025-03-28 01:25:55 +0100

    Sprint-2
    environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    // Ajoute ici toutes les autres variables spécifiques à la production
+  };  
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    // Ajoute ici toutes les variables spécifiques à l'environnement de développement
+  };
