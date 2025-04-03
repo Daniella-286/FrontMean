aad94f8307f42d961b94e0519555b05a1b18ab93
Daniella-286 <119035286+Daniella-286@users.noreply.github.com>
2025-04-03 18:35:15 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -74,9 +74,9 @@ import { MouvementStockComponent } from './components/mecanicien/mouvement-stock
 import { PlanningComponent } from './components/mecanicien/planning/planning.component';
 
 export const routes: Routes = [
- { path: '', redirectTo: 'connexion', pathMatch: 'full' } ,
+ { path: '', redirectTo: '', pathMatch: 'full' } ,
 
- { path: 'connexion', component: ConnexionComponent },
+ { path: '', component: ConnexionComponent },
  { path: 'services', component: ServiceListComponent }, 
  { path: 'connexion-manager', component: ConnexionManagerComponent },
 
@@ -146,6 +146,6 @@ export const routes: Routes = [
  { path: 'factures-parking-du-jour', component: FactureParkingComponent },
  { path: 'mouvement-stock', component: MouvementStockComponent },
 
- { path: '**', redirectTo: 'connexion'}
+ { path: '**', redirectTo: ''}
  
 ];
